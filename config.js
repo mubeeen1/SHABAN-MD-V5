57a5f087cee00296d7d871b77a692929a1bde12f
Mubeen Nasir <112949359+mubeeen1@users.noreply.github.com>
2025-06-11 14:13:31 +0500

    Added new session id

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~jBdDyLiS#xdHyU5bWN02vThFJlLRMAdZBreTbl8CAR-uBSD7Ay4E",
+SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~zANmGKrA#vjdGnUt-llJZ8lNMkNVI-VUPcXbXPRLMG30d6uKPc-4",
 // add your Session Id
 AUTO_BIO: process.env.AUTO_BIO || "false", 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
@@ -46,7 +46,7 @@ LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *SHABAN-MD*⚡",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
-AUTO_REACT: process.env.AUTO_REACT || "true",
+AUTO_REACT: process.env.AUTO_REACT || "false",
 // make this true or false for auto react on all msgs
 ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words  

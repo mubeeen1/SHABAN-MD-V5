07341f113fc65ab5de73e75a81d62837350d3cdc
Mubeen Nasir <112949359+mubeeen1@users.noreply.github.com>
2025-06-13 10:27:51 +0500

    Update config.js

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ STICKER_NAME: process.env.STICKER_NAME || "SHABAN-MD",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
-CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || " ",
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 

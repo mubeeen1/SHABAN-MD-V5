4a8e71a0e81e5be2d8667b2543369a6584e82193
Mubeen Nasir <112949359+mubeeen1@users.noreply.github.com>
2025-06-14 08:14:33 +0500

    Update config.js

--- config.js
+++ config.js
@@ -54,7 +54,7 @@ MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
 ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
 // make true for send automatic voices
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 // make true for automatic stickers 
